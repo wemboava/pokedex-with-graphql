9db3ff942e208cf933a09f86e9337692a894ba74
willian emboava <w.emboava.o@gmail.com>
2020-07-06 15:42:18 -0300

    added margin auto

--- src/components/notFound/styles.js
+++ src/components/notFound/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  margin: auto;
   max-width: 968px;
   display: flex;
   flex-direction: column;
