01809e6d40220b2bbe1d427ef33775a861c234dd
willian emboava <w.emboava.o@gmail.com>
2020-07-06 15:40:36 -0300

    removed useHistory

--- src/components/pokemonsList/index.js
+++ src/components/pokemonsList/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { Container, List, Item, Types, TypeName } from "./styles";
 
 const TypesList = ({ pokemons }) => {
-  const history = useHistory();
-
   return (
     <Container>
       <List>
         {pokemons.map((pokemon) => (
           <Link key={pokemon.id} to={`/pokemon/${pokemon.name}`}>
-            <Item
-              onClick={() => history.push(`/pokemon/${pokemon.name}`)}
-              bgColor={pokemon.bgColor || "#c4c4c4"}
-            >
+            <Item bgColor={pokemon.bgColor || "#c4c4c4"}>
               <strong>{pokemon.name}</strong>
               <div>
                 <img src={pokemon.image} alt={pokemon.name} />
